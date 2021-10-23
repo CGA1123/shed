442e09954565bf14c515e2450ea7922ec5d341a6
Christian Gregg <christian@bissy.io>
2021-10-23 12:14:52 +0100

    use async/block pg api

--- ruby/lib/shed/active_record.rb
+++ ruby/lib/shed/active_record.rb
@@ -68,44 +68,128 @@ module Shed
     end
 
     # {PostgreSQLStatementTimeout} is intended to be prepended to
-    # `ActiveRecord::ConnectionAdapters::PostgreSQLAdapter`. It wraps all
-    # all individual database queries with calls to set and reset the
-    # `statement_timeout` variable to {Shed.time_left_ms} before execution.
+    # `ActiveRecord::ConnectionAdapters::PostgreSQLAdapter`.
+    #
+    # It executes all calls via ruby-pg/libpq's async API before blocking with
+    # a timeout. If a timeout is exceeded, the in progress query will be
+    # cancelled, raising a PG::QueryCanceled error.
     module PostgreSQLStatementTimeout
-      def execute(sql, name = nil)
-        with_timeout { super }
-      end
+      def query(sql, name = nil)
+        return super unless Shed.timeout_set?
 
-      def execute_and_clear(sql, name, binds, prepare: false, async: false)
-        with_timeout { super }
-      end
-
-      private
-
-      def with_timeout(&block)
         time_left_ms = Shed.time_left_ms
         Shed.ensure_time_left!
 
-        begin
-          # TODO: is using @connection.execute ok (?)
-          @connection.execute("SET SESSION statement_timeout TO #{time_left_ms}")
+        materialize_transactions
+        mark_transaction_written_if_write(sql)
 
-          block.call
-        ensure
-          if default_statement_timeout
-            @connection.execute("SET SESSION statement_timeout TO #{default_statement_timeout}")
-          else
-            @connection.execute("SET SESSION statement_timeout TO DEFAULT")
+        log(sql, name) do
+          ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
+            @connection.send_query(sql)
+
+            shed_await_result(@connection, timeout_ms: time_left_ms)
+              .map_types!(@type_map_for_results)
+              .values
           end
         end
       end
 
-      def default_statement_timeout
-        return @default_statement_timeout if defined?(@default_statement_timeout)
+      def execute(sql, name = nil)
+        return super unless Shed.timeout_set?
 
-        variables = @config.fetch(:variables, {})
+        sql = transform_query(sql)
+        check_if_write_query(sql)
 
-        @default_statement_timeout = variables[:statement_timeout] || variables["statement_timeout"]
+        materialize_transactions
+        mark_transaction_written_if_write(sql)
+
+        log(sql, name) do
+          ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
+            @connection.send_query(sql)
+
+            shed_await_result(@connection, timeout_ms: time_left_ms)
+          end
+        end
+      end
+
+      def exec_no_cache(sql, name, binds, async: false)
+        return super unless Shed.timeout_set?
+
+        time_left_ms = Shed.time_left_ms
+        Shed.ensure_time_left!
+
+        materialize_transactions
+        mark_transaction_written_if_write(sql)
+
+        # make sure we carry over any changes to ActiveRecord.default_timezone
+        # that have been made since we established the connection
+        update_typemap_for_default_timezone
+
+        type_casted_binds = type_casted_binds(binds)
+        log(sql, name, binds, type_casted_binds, async: async) do
+          ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
+            @connection.send_query_params(sql, type_casted_binds)
+
+            shed_await_result(@connection, timeout_ms: time_left_ms)
+          end
+        end
+      end
+
+      def exec_cache(sql, name, binds, async: false)
+        return super unless Shed.timeout_set?
+
+        time_left_ms = Shed.time_left_ms
+        Shed.ensure_time_left!
+
+        materialize_transactions
+        mark_transaction_written_if_write(sql)
+        update_typemap_for_default_timezone
+
+        stmt_key = prepare_statement(sql, binds)
+        type_casted_binds = type_casted_binds(binds)
+
+        log(sql, name, binds, type_casted_binds, stmt_key, async: async) do
+          ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
+            @connection.send_query_prepared(stmt_key, type_casted_binds)
+
+            shed_await_result(@connection, timeout_ms: time_left_ms)
+          end
+        end
+      rescue ActiveRecord::StatementInvalid => e
+        raise unless is_cached_plan_failure?(e)
+
+        # Nothing we can do if we are in a transaction because all commands
+        # will raise InFailedSQLTransaction
+        if in_transaction?
+          raise ActiveRecord::PreparedStatementCacheExpired.new(e.cause.message)
+        else
+          @lock.synchronize do
+            # outside of transactions we can simply flush this query and retry
+            @statements.delete sql_key(sql)
+          end
+          retry
+        end
+      end
+
+      private
+
+      # {shed_await_result} will block waiting for the connection to return a
+      # result for up to {timeout_ms}.
+      #
+      # @param cnx [PG::Connection] the connection to the database.
+      # @param timeout_ms: [Numeric] the timeout (in milliseconds) to block for.
+      # @raise [Shed::Timeout] if the timeout is exceeded.
+      def shed_await_result(cnx, timeout_ms:)
+        if cnx.block(timeout_ms / 1000.0) # seconds
+          cnx.get_last_result
+        else
+          begin
+            cnx.cancel
+            cnx.get_last_result
+          rescue PG::QueryCanceled
+            raise Shed::Timeout
+          end
+        end
       end
     end
 
